2dafb8d08264fd6009a41960b9954a2f05c41451
acrisiodejesus <acrisiodejesus@gmail.com>
2025-08-14 19:54:51 +0200

    Api Env Variable

--- src/config/api.js
+++ src/config/api.js
@@ -1,12 +1,13 @@
-import axios from "axios"
+import axios from "axios";
 
 const token = localStorage.getItem("token");
-const api = () => axios.create({
-    baseURL: "http://127.0.0.1:5000/api",
+const api = () =>
+  axios.create({
+    baseURL: process.env.REACT_APP_API_URL,
     headers: {
-        Authorization: token ? `Bearer ${token}` : "",
-        "Content-Type": "application/json",
-    }
-});
+      Authorization: token ? `Bearer ${token}` : "",
+      "Content-Type": "application/json",
+    },
+  });
 
 export default api;
