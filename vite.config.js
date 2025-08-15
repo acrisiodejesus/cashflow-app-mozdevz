55db68d078f9c471bfa7ab9bae4b4dde64adb257
acrisiodejesus <acrisiodejesus@gmail.com>
2025-08-15 10:30:42 +0200

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    allowedHosts: ["jcwsgwcccooggww4ckgwg4c8.95.216.50.19.sslip.io"],
+  },
+});
